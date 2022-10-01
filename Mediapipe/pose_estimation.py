72adf14fa8fe2c6ea08835d76473e644ec8c933e
jack719 <jacklih953@gmail.com>
2022-10-01 20:46:20 +0800

    1001

--- Mediapipe/pose_estimation.py
+++ Mediapipe/pose_estimation.py
@@ -8,3 +8,4 @@ from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+#%%
