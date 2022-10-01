f3da2f6edcd205143b048e3c68a2ba74a88bf4b7
jack719 <jacklih953@gmail.com>
2022-10-01 20:43:39 +0800

    歌仔戲

---
+++ Mediapipe/pose_estimation.py
@@ -0,0 +1,10 @@
+import cv2
+import mediapipe as mp
+import numpy
+import os
+
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
